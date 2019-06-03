3fa482afe9bfd12167155222a7cb35afd906307f
Alistair Smith <alistairsmith@flightofstairs.org>
2019-06-04 00:34:18 +0100

    Configure detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    id("io.gitlab.arturbosch.detekt").version("1.0.0-RC14")
 }
 
 repositories {
@@ -11,4 +12,19 @@ dependencies {
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.0.0-RC14")
+}
+
+detekt {
+    toolVersion = "1.0.0-RC14"
+    input = files("src/main/kotlin", "src/test/kotlin")
+    filters = ".*/resources/.*,.*/build/.*"
+
+    config = files(projectDir.resolve("detekt-config.yml"))
+    buildUponDefaultConfig = true
+
+    baseline = projectDir.resolve("detekt-baseline.xml")
+
+    failFast = false
 }
